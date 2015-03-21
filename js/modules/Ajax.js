ac4d542a1193444b578e3249a589223f56db2967
Gosha Krikun <gosha@krikun.org>
2015-03-21 14:23:59 +0400

    dont know how to do merge with another branch from ci

---
+++ js/modules/Ajax.js
@@ -0,0 +1,18 @@
+// XMLHttpRequest
+// http://en.wikipedia.org/wiki/XMLHttpRequest
+var Ajax = function () {
+  if (typeof XMLHttpRequest === 'undefined') {
+  XMLHttpRequest = function() {
+    try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+    catch(e) {}
+    try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+    catch(e) {}
+    try { return new ActiveXObject("Msxml2.XMLHTTP"); }
+    catch(e) {}
+    try { return new ActiveXObject("Microsoft.XMLHTTP"); }
+    catch(e) {}
+    throw new Error("This browser does not support XMLHttpRequest.");
+  };
+  }
+  return new XMLHttpRequest();
+};
